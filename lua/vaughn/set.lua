6e1ca2facaf5107ee7e64197cac5b30ccf17e768
Trevor Vaughn Hauck <hauck.trevor@gmail.com>
2023-04-28 22:26:06 -0400

    relative line numbers reenabled

--- lua/vaughn/set.lua
+++ lua/vaughn/set.lua
@@ -1,6 +1,6 @@
 vim.opt.guicursor="n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 vim.opt.nu = true
---vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
