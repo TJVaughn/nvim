e587bc3584fc549eb77417379c104c2e0df89052
Trevor Vaughn Hauck <hauck.trevor@gmail.com>
2023-06-02 08:46:40 -0400

    fix harpoon file nav

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,12 +4,11 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-
-vim.keymap.set("n", "<C-6>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-7>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-8>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-9>", function() ui.nav_file(4) end)
-vim.keymap.set("n", "<C-0>", function() ui.nav_file(5) end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-p>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(5) end)
 
 require("harpoon").setup({
     menu = {
