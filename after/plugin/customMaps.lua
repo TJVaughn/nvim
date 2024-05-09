d214a155bf3a81d282f2947102d4b0389766c1f5
vaughn <hauck.trevor@gmail.com>
2024-05-09 11:36:20 -0400

    add easy save command

--- after/plugin/customMaps.lua
+++ after/plugin/customMaps.lua
@@ -2,4 +2,4 @@ vim.api.nvim_set_keymap("n", "<leader>=", "i()=>{}", {noremap=true, silent=true}
 
 vim.api.nvim_set_keymap("n", "<leader>tc", "itry {<Enter><Enter>} catch (error) {<Enter><Enter>}", {noremap=true, silent=true})
 
-
+vim.api.nvim_set_keymap("i", "<c-s>", "<Esc>:w<Enter>", {noremap=true, silent=true})
