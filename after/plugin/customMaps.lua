c5a503b9d87b23113760ba5ed3175c7f474938e6
vaughn <hauck.trevor@gmail.com>
2024-06-14 16:10:37 -0400

    Add markdown image shortcut

--- after/plugin/customMaps.lua
+++ after/plugin/customMaps.lua
@@ -3,3 +3,6 @@ vim.api.nvim_set_keymap("n", "<leader>=", "i()=>{}", {noremap=true, silent=true}
 vim.api.nvim_set_keymap("n", "<leader>tc", "itry {<Enter><Enter>} catch (error) {<Enter><Enter>}", {noremap=true, silent=true})
 
 vim.api.nvim_set_keymap("i", "<c-s>", "<Esc>:w<Enter>", {noremap=true, silent=true})
+
+vim.api.nvim_set_keymap("n", "<leader>mdi", "i![]()<left><left><left>", {noremap=true, silent=true})
+
